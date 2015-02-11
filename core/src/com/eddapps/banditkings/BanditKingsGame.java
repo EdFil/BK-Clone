f57fb89e34217de5db009e6a66a6fedf00a60860
Edgar Filipe Ramires dos Santos <edfil221@gmail.com>
2015-02-11 21:11:15 +0000

    Added new textures and debug for colliders that don't exist

--- core/src/com/eddapps/banditkings/BanditKingsGame.java
+++ core/src/com/eddapps/banditkings/BanditKingsGame.java
@@ -4,53 +4,54 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class BanditKingsGame extends ApplicationAdapter {
 	
+	ShapeRenderer mShapeRenderer;
 	SpriteBatch mSpriteBatch;
 	
-	Texture mBackground;
-	Texture mCastleLeft;
-	TextureRegion mCastleRight;
+	CastleLeft mCastleLeft;
+	CastleRight mCastleRight;
 	
 	OrthographicCamera mCamera;
 	
 	@Override
 	public void create () {
+		mShapeRenderer = new ShapeRenderer();
 		mSpriteBatch = new SpriteBatch();
 		mCamera = new OrthographicCamera();
 		mCamera.setToOrtho(false, 800, 480);
+
+		mCastleLeft = new CastleLeft();
+		mCastleRight = new CastleRight();
 		
-		mBackground = new Texture(Gdx.files.internal("background.png"));
-		mCastleLeft = new Texture(Gdx.files.internal("castle.png"));
-		mCastleRight = new TextureRegion(mCastleLeft);
-		mCastleRight.flip(true, false);
 	}
 
 	@Override
 	public void render () {
 		mCamera.update();
 				
-		Gdx.gl.glClearColor(0.0f, 0.0f, 0.5f, 1.0f);
+		Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 	    mSpriteBatch.setProjectionMatrix(mCamera.combined);
 	    mSpriteBatch.begin();
-		mSpriteBatch.draw(mBackground, 0, 0);
-		mSpriteBatch.draw(mCastleLeft, 0-mCastleLeft.getWidth()/2, 0);
-		mSpriteBatch.draw(mCastleRight, Gdx.graphics.getWidth()-mCastleLeft.getWidth()/2, 0);
+	    
+		mCastleLeft.draw(mSpriteBatch);
+		mCastleRight.draw(mSpriteBatch);
+		
         mSpriteBatch.end();
+        
+        mShapeRenderer.setProjectionMatrix(mCamera.combined);
+        mShapeRenderer.begin(ShapeType.Line);
+        
+        mCastleLeft.debugDraw(mShapeRenderer);
+        mCastleRight.debugDraw(mShapeRenderer);
+        
+        mShapeRenderer.end();
 		
 	}
 }
